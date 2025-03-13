aa5f89b5e0ff2dccd4499a6dbc2f4c64af017c12
emreyilldirrm <emreyl.yldrm@yandex.com>
2025-03-13 14:54:11 +0300

    prediction

--- streamlit_app/utils.py
+++ streamlit_app/utils.py
@@ -2,4 +2,8 @@ import joblib
 
 def load_model():
     """Kaydedilmiş makine öğrenmesi modelini yükler."""
-    return joblib.load("model.pkl")
+    import os
+    # Mevcut çalışma dizinini kontrol et
+    current_dir = os.getcwd()
+    model_path = os.path.join(current_dir, "streamlit_app", "model.pkl")
+    return joblib.load(model_path)
